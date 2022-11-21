c5734d88a239b1c1a4c0c5bc7f7e3d844b444ba5
Thomas Regourd <tom.reg@hotmail.fr>
2022-11-21 15:51:56 +0100

    initial commit with theme setup

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+
+const data = {
+  sub_heading: "GameOn",
+  heading: "The Next Generation Web 3.0 IGO Launchpad",
+  content: "Full-scale Blockchain Gaming Ecosystem for IGOs & NFT Pre-Sales",
+};
+
+function Hero() {
+  const [initData, setInitData] = useState({});
+
+  useEffect(() => {
+    setInitData(data);
+  });
+
+  return (
+    <section className="hero-section">
+      <div className="container">
+        <div className="row align-items-center justify-content-center">
+          <div className="col-12 col-md-6 col-lg-9 text-center">
+            {/* Hero Content */}
+            <div className="hero-content">
+              <div className="intro text-center mb-5">
+                <span className="intro-text">{initData.sub_heading}</span>
+                <h1 className="mt-4">{initData.heading}</h1>
+                <p>{initData.content}</p>
+              </div>
+              {/* Buttons */}
+              <div className="button-group">
+                <a
+                  className="btn btn-bordered active smooth-anchor"
+                  href="#explore"
+                >
+                  <i className="icon-rocket mr-2" />
+                  Explore IGOs
+                </a>
+                <a className="btn btn-bordered-white" href="/apply">
+                  <i className="icon-note mr-2" />
+                  Apply Now
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
